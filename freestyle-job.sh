709fbbf2cf9bb21848bc3987196bfb545cac4c20
Atiyaali <atiyaali653@gmail.com>
2026-06-04 23:05:56 +0500

    .

--- freestyle-job.sh
+++ freestyle-job.sh
@@ -2,7 +2,13 @@
 # node --version
 # echo "hello this is atiya"
 echo "building front image"
-docker build -t wandarlustfrontendjenkins:latest ./frontend
+docker build -t atiyadocker/wandarlustfrontendjenkins:latest ./frontend
 echo "building back image"
-docker build -t wandarlustbackendjenkins:latest ./backend
+docker build -t atiyadocker/wandarlustbackendjenkins:latest ./backend
 echo "done both"
+echo  "loging"
+docker login -u $USERNAME -p $PASSWORD
+echo "pushing frontend image"
+docker push atiyadocker/wandarlustfrontendjenkins:latest
+echo "pushing backend image"
+docker push atiyadocker/wandarlustbackendjenkins:latest
