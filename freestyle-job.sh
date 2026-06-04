5ab14564d8a3968cec5f992d5e48de3e037176e8
Atiyaali <atiyaali653@gmail.com>
2026-06-05 01:02:16 +0500

    .

--- freestyle-job.sh
+++ freestyle-job.sh
@@ -5,7 +5,7 @@ echo "building front image"
 # docker build -t atiyadocker/wandarlustfrontendjenkins:latest ./frontend
 docker build -t 127.0.0.1:8083/wandarlustfrontendjenkins:latest ./frontend
 echo "building back image"
-docker build -t 27.0.0.1:8083/wandarlustbackendjenkins:latest ./backend
+docker build -t 127.0.0.1:8083/wandarlustbackendjenkins:latest ./backend
 echo "done both"
 echo  "loging"
 echo $PASSWORD | docker login -u $USERNAME --password-stdin 127.0.0.1:8083
