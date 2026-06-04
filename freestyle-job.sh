c54f0cac3ca9020a04147a29668dceaa7fb15dcb
Atiyaali <atiyaali653@gmail.com>
2026-06-04 21:54:50 +0500

    .

--- freestyle-job.sh
+++ freestyle-job.sh
@@ -1,3 +1,8 @@
-php --version
-node --version
-echo "hello this is atiya"
+# php --version
+# node --version
+# echo "hello this is atiya"
+echo "building front image"
+docker build -t wandarlustfrontendjenkins:latest ./frontend
+echo "building back image"
+docker build -t wandarlustbackendjenkins:latest ./backend
+echo "done both"
